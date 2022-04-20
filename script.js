5769c87261ea29db61a5ed823c41f0bad7710f69
sachabate <abate@fieldsafesolutions.com>
2022-04-20 17:07:42 -0600

    feat: build and render grid

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // JavaScript script
+const GRID_SIZE = 16;
+
+const grid = document.querySelector('#grid');
+for (let index = 0; index < GRID_SIZE * GRID_SIZE; index++) {
+    const div = document.createElement('div');
+    div.classList.add('gridElement');
+    grid.appendChild(div);
+}
