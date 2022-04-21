e8ebb9092d49f971a22833984270724dce3de743
sachabate <abate@fieldsafesolutions.com>
2022-04-20 21:24:40 -0600

    feat: grid drawing and painting
    Grid can resize itself appropriately for any number of squares, and
    squares may be painted with mouseclick.

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 // JavaScript script
-const GRID_SIZE = 16;
+const GRID_SIZE = 50;
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 const grid = document.querySelector('#grid');
-for (let index = 0; index < GRID_SIZE * GRID_SIZE; index++) {
+for (let index = 0; index < GRID_SIZE; index++) {
     const div = document.createElement('div');
-    div.classList.add('gridElement');
+    div.classList.add('column');
+    div.classList.add(index);
+
+    for (let i = 0; i < GRID_SIZE; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('gridElement');
+        gridElement.addEventListener('mouseover', paint);
+        gridElement.addEventListener('mousedown', paint);
+        div.appendChild(gridElement);
+    }
     grid.appendChild(div);
+}
+
+function paint(e) {
+    if (e.type === 'mouseover' && !mouseDown) {return};
+    e.target.style.backgroundColor = 'black';
 }
